cce893aafe3821adfab6234bdd915f3c1cebb106
Osipov_Daniil <334924@niuitmo.ru>
2024-08-26 16:51:02 +0300

    add qr generating

--- src/main/java/com/t1project/club_card/controllers/ClubMemberController.java
+++ src/main/java/com/t1project/club_card/controllers/ClubMemberController.java
@@ -1,10 +1,18 @@
 package com.t1project.club_card.controllers;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.t1project.club_card.dto.ChangeFieldDTO;
 import com.t1project.club_card.services.ClubMemberService;
 import com.t1project.club_card.models.ClubMember;
+import com.t1project.club_card.services.QRCodeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -17,9 +25,23 @@ public class ClubMemberController {
     @Autowired
     private ClubMemberService clubMemberService;
 
+    @Autowired
+    private QRCodeService qrCodeService;
+
     @GetMapping("")
     public ClubMember getCurrentClubMember(@AuthenticationPrincipal ClubMember clubMember) {
         return clubMember;
+    }
+
+    @GetMapping("/qr")
+    public Mono<ResponseEntity<byte[]>> getQR(@AuthenticationPrincipal ClubMember clubMember) {
+        return qrCodeService.generateQRCodeForClubMember(clubMember, 200, 200)
+                .map(qrCode -> {
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.setContentType(MediaType.IMAGE_PNG);
+                    headers.setContentLength(qrCode.length);
+                    return new ResponseEntity<>(qrCode, headers, HttpStatus.OK);
+                }).onErrorResume(e -> Mono.just(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR)));
     }
 
     @PostMapping("/change-email")
