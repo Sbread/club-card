f23d0fb8549abc39ccd77bdb3f549362f2a877c7
Osipov_Daniil <334924@niuitmo.ru>
2024-08-26 17:28:28 +0300

    fix view error

--- src/main/java/com/t1project/club_card/controllers/AuthController.java
+++ src/main/java/com/t1project/club_card/controllers/AuthController.java
@@ -15,12 +15,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-@Controller
+@RestController
 public class AuthController {
 
     @Autowired
